8d39286ba9b2e8594e577939fbc6d87c09f4b508
Ofer Ben-Yacov <ofer.benyacov@gmail.com>
2016-01-19 15:39:18 +0200

    Add inter-cloud support
    The goal of this patch is to enable connection between multiple
    overlay network in deferent openstack clouds
    
    This patch includes PEP8 fixes.
    
    Change-Id: I082aa1e8bc575fa84d5fe692ea5303284c37fc2b

--- networking_l2gw/services/l2gateway/exceptions.py
+++ networking_l2gw/services/l2gateway/exceptions.py
@@ -120,3 +120,11 @@ base.FAULT_MAP.update({L2GatewayInUse: web_exc.HTTPConflict,
                        L2MultipleGatewayConnections: web_exc.HTTPConflict,
                        L2GatewayDuplicateSegmentationID: web_exc.HTTPConflict,
                        OVSDBError: web_exc.HTTPConflict})
+
+
+class L2RemoteGatewayNotFound(exceptions.NotFound):
+    message = _("The remote gateway %(id)s not found")
+
+
+class L2RemoteGatewayConnectionNotFound(exceptions.NotFound):
+    message = _("The remote gateway connection %(id)s not found")
